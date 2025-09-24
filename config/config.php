509b1a8f2d590fb5f5555e87839bd3c7c97fce9f
noorkhaled <49693643+noorkhaled@users.noreply.github.com>
2025-09-25 02:49:11 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 return [
     'env'      => 'local',
     'db_host'  => '127.0.0.1',
-    'db_name'  => 'amit_task', // change it to the name you prefer in your schema
+    'db_name'  => 'task_manager', // change it to the name you prefer in your schema
     'db_user'  => 'root',
     'db_pass'  => '',
     'log_path' => __DIR__ . '/../logs/app.log',
